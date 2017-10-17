438a72bc96a600e1d30cd685c0b072aa67e0dd92
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-10-17 17:19:42 -0500

    Added first person hands

--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShooting : MonoBehaviour {
+public class PlayerShooting : MonoBehaviour
+{
+    public float speed = 1000;
+    public float fireFrequency;
 
-	// Use this for initialization
-	void Start () {
-		
+    bool isFiring;
+    bool readyToFire;
+
+    Camera myCamera;
+    RaycastHit hit;
+
+    void Start ()
+    {
+        readyToFire = true;
+        myCamera = Camera.main;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        isFiring = PlayerInput.isFiring;
+
+        if(isFiring && readyToFire)
+        {
+            readyToFire = false;
+            StartCoroutine(FireFrequency());
+        }
+    }
+
+    IEnumerator FireFrequency()
+    {
+        yield return new WaitForSeconds(fireFrequency);
+        readyToFire = true;
+    }
 }
