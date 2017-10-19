53446efbf86ddae90f7ef4a7d25bc57fee079e09
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-10-18 21:19:31 -0500

    post process

---
+++ Assets/Game/Scripts/JetPackBob.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetPackBob : MonoBehaviour
+{
+    public float zClampValue;
+    public float xClampValue;
+    public float zSpeed;
+    public float xSpeed;
+
+    float zRotation;
+    float xRotation;
+    float horizontal;
+    float horizontal2;
+
+    float vertical;
+    float vertical2;
+
+    Quaternion gunRotation;
+
+    void Update ()
+    {
+        horizontal = PlayerInput.horizontal;
+        horizontal2 = PlayerInput.horizontal2;
+
+        vertical = PlayerInput.vertical;
+        vertical2 = PlayerInput.vertical2;
+
+        zRotation += -horizontal * zSpeed * Time.fixedDeltaTime;
+        zRotation += -horizontal2 * zSpeed * Time.fixedDeltaTime;
+        zRotation = ClampAngle(zRotation, -zClampValue, zClampValue);
+
+        xRotation += vertical * xSpeed * Time.fixedDeltaTime;
+        xRotation += vertical2 * xSpeed * Time.fixedDeltaTime;
+        xRotation = ClampAngle(xRotation, -xClampValue, xClampValue);
+
+        gunRotation = Quaternion.Euler(xRotation, 0, zRotation);
+        //transform.rotation = gunRotation;
+
+        if (horizontal != 0 || horizontal2 != 0 || vertical != 0 || vertical2 != 0)
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, gunRotation, 1);
+        else if(horizontal == 0 && horizontal2 == 0 || vertical == 0 && vertical2 == 0)
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.identity, 1);
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360.0f)
+            angle += 360.0f;
+        if (angle > 360.0f)
+            angle -= 360.0f;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
